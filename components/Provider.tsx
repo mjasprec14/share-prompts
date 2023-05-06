075971ed709d35720920aab924d5bc67c53e4cda
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-06 20:59:33 +0800

    feat: applying Provider in layout

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,3 +1,14 @@
-export default function Provider() {
-  return <div>Provider</div>;
+'use client';
+
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+
+export default function Provider({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session?: Session;
+}) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
