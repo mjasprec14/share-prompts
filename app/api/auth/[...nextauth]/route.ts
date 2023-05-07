66075be8749983390c821a5e0b39ece35c304367
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-07 11:26:56 +0800

    feat: apply sign in, create new user, and session logic

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 import { connectToDB } from '@utils/database';
+import User from '@models/user';
 
 const handler = NextAuth({
   providers: [
@@ -10,14 +11,38 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  async session({ session }: { session: any }) {},
-  async signIn({ profile }: { profile: string }) {
+  async session({
+    session,
+  }: {
+    session: { user: { email: string; id: string } };
+  }) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
+  async signIn({
+    profile,
+  }: {
+    profile: { email: string; name: string; picture: any };
+  }) {
     try {
       await connectToDB();
 
       // check if user already exist
+      const userExists = await User.findOne({ email: profile.email });
 
       // create new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
     } catch (error) {
       console.log('Failed to Sign In: ', error);
 
