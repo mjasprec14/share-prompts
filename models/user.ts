48ebcfeaed692df1e7658a58bd77717c8f8dd89c
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-07 18:33:23 +0800

    fix: console error message for invalid username type

--- models/user.ts
+++ models/user.ts
@@ -11,7 +11,7 @@ const userSchema = new Schema({
     required: [true, 'Username is required.'],
     match: [
       /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-      'Username invalid, it mshould contain 8-20 alphanumeric letters and be unique.',
+      'Username invalid, it should contain 8-20 alphanumeric letters and be unique.',
     ],
   },
   image: {
