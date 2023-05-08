55118f86eade904e035823fc5789757ba42b870b
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-08 21:02:20 +0800

    fix: Renamed CurrentProfile component to UserProfile

---
+++ components/UserProfile.tsx
@@ -0,0 +1,11 @@
+import { Profile } from '@typings';
+
+export default function UserProfile({
+  name,
+  desc,
+  data,
+  handleEdit,
+  handleDelete,
+}: Profile) {
+  return <div>CurrentProfile</div>;
+}
