7ee45ba0a2ccbf5b916c3304c2a1e6b8f3081fd7
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-08 21:43:35 +0800

    feat: user profile display prompts styles

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,4 +1,5 @@
 import { Profile } from '@typings';
+import PromptCard from './PromptCard';
 
 export default function UserProfile({
   name,
@@ -7,5 +8,23 @@ export default function UserProfile({
   handleEdit,
   handleDelete,
 }: Profile) {
-  return <div>CurrentProfile</div>;
+  return (
+    <section className='w-full'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{name}</span> Profile
+      </h1>
+      <p className='desc text-left'>{desc}</p>
+
+      <div className='mt-10 prompt_layout'>
+        {data.map((prompt, index) => (
+          <PromptCard
+            key={index}
+            prompt={prompt}
+            handleEdit={() => handleEdit && handleEdit(prompt)}
+            handleDelete={() => handleDelete && handleDelete(prompt)}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
