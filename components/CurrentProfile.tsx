f99808403ccc0c14cf3ae58a2ef301539fb9de27
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-08 18:36:07 +0800

    feat: CurrentProfile component

---
+++ components/CurrentProfile.tsx
@@ -0,0 +1,11 @@
+import { CurrentProfileType } from '@typings';
+
+export default function CurrentProfile({
+  name,
+  desc,
+  data,
+  handleEdit,
+  handleDelete,
+}: CurrentProfileType) {
+  return <div>CurrentProfile</div>;
+}
