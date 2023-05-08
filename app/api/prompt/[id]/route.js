82cf9d0b156b9e144f33f5ea7f2b7ef126673f5c
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-08 22:33:47 +0800

    feat: Get and Patch Prompt via ID

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,36 @@
+import { connectToDB } from '@utils/database';
+import Prompt from '@models/prompt';
+
+export async function GET({ params }) {
+  try {
+    await connectToDB();
+
+    const prompt = await Prompt.findById(params.id).populate('creator');
+
+    if (!prompt) return new Response('Prompt not found', { status: 404 });
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response('Failed to fetch all prompt', { status: 500 });
+  }
+}
+
+export async function PATCH(request, { params }) {
+  const { prompt, tag } = await request.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt)
+      return new Response('Prompt not found', { status: 404 });
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+  } catch (error) {
+    new Response('Failed to update Prompt', { status: 500 });
+  }
+}
